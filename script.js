063da2231d256e2dbb888c72b39c65846ad5dbd7
jiteshBalani <120266890+JiteshBalani@users.noreply.github.com>
2023-07-19 18:04:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,16 +24,6 @@ const getWeather = (city) => {
         .then(response => response.json())
         .then(response => {
             console.log(response)
-            // cloud_pct.innerHTML = response.cloud_pct
-            // temp.innerHTML = response.temp
-            // feels_like.innerHTML = response.feels_like
-            // humidity.innerHTML = response.humidity
-            // min_temp.innerHTML = response.min_temp
-            // max_temp.innerHTML = response.max_temp
-            // wind_speed.innerHTML = response.wind_speed
-            // // wind_degrees.innerHTML = response.wind_degrees
-            // sunrise.innerHTML = response.sunrise
-            // sunset.innerHTML = response.sunset
             cloud_pct.innerHTML = response.cloud_pct;
             temp.innerHTML = response.temp;
             feels_like.innerHTML = response.feels_like;
@@ -48,28 +38,28 @@ const getWeather = (city) => {
         .catch(err => console.error(err))
 }
 
-function populateTable(cities) {
-    const tbody = document.querySelector("tbody");
-    const tr = tbody.querySelectorAll("tr");
-    for (let i = 0; i < tr.length; i++) {
-        fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + cities[i], options)
-            .then(response => response.json())
-            .then(response => {
-                let currentRow = tr[i];
-                const td = currentRow.querySelectorAll("td");
-                for (let j = 0; j < td.length; j++) {
-                    let currentTd = td[j];
-                    let currentSpan = currentTd.firstChild;
-                    let className = currentSpan.classList[0];
-                    // console.log(response.className);
-                    // currentSpan.innerText = '25';
-                    currentSpan.innerText = response[className];
-                }
-            })
+// function populateTable(cities) {
+//     const tbody = document.querySelector("tbody");
+//     const tr = tbody.querySelectorAll("tr");
+//     for (let i = 0; i < tr.length; i++) {
+//         fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + cities[i], options)
+//             .then(response => response.json())
+//             .then(response => {
+//                 let currentRow = tr[i];
+//                 const td = currentRow.querySelectorAll("td");
+//                 for (let j = 0; j < td.length; j++) {
+//                     let currentTd = td[j];
+//                     let currentSpan = currentTd.firstChild;
+//                     let className = currentSpan.classList[0];
+//                     // console.log(response.className);
+//                     // currentSpan.innerText = '25';
+//                     currentSpan.innerText = response[className];
+//                 }
+//             })
 
-    }
-}
-populateTable(["Bengaluru", "Hyderabad", "Indore", "Kolkata"]);
+//     }
+// }
+// populateTable(["Bengaluru", "Hyderabad", "Indore", "Kolkata"]);
 
 
 
@@ -81,10 +71,7 @@ submit.addEventListener("click", (e) => {
 })
 
 getWeather("New Delhi");
-// getWeather("Bengaluru");
-// getWeather("Hyderabad");
-// getWeather("Indore");
-// getWeather("Kolkata");
+
 
 
 
